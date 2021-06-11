52ddec02c77f1703b849f9729aa6a82565fe9270
geraldiner <hello@geraldiner.com>
2021-06-10 18:51:40 -1000

    work on chunkArray

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -1,12 +1,37 @@
 /* 
- Given two or more arrays, write a function that combines
- their elements into one array without any repetition. 
- E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
+Given an array and a chunk size, divide the array into many sub-arrays where each sub-array has the specified length. E.g chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5)
+// should return [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]
 */
 
+// function chunkArray(array, size) {
+//     let newArr = []
+//     let temp = []
+//     for (let i = 0; i < array.length; i++) {
+//         if ((i + 1) % size == 0) {
+//             temp.push(array[i])
+//             newArr.push(temp)
+//             temp = []
+//         } else {
+//             temp.push(array[i])
+//         }
+//     }
+//     newArr.push(temp)
+//     return newArr
+// }
+
 function chunkArray(array, size) {
-    // Code goes here
+    let result = []
+    for (value of array) {
+        let lastArray = result[result.length - 1]
+        if (!lastArray || lastArray.length == size) {
+            result.push([value])
+        } else {
+            lastArray.push(value)
+        }
+    }
+    return result
 }
 
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5))
 
 module.exports = chunkArray
