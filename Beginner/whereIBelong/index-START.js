19dd4a1434f6909aef396dfaa5d157bd1509819d
Geraldine <hello@geraldiner.com>
2021-06-30 21:50:23 -1000

    finish whereIBelong

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -7,13 +7,42 @@ whereIBelong([1,2,3,4], 1.5) // should return 1 because it is greater
 than 1(index 0), but less than 2(index 1).
 */
 
-
+/* function whereIBelong(arr, num) {
+  arr.sort((a, b) => a - b);
+  if (arr.length == 0 || num < arr[0]) {
+    return 0;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (num > arr[i] && num < arr[i + 1]) {
+      return i + 1;
+    }
+  }
+  return arr.length;
+} */
 
 function whereIBelong(arr, num) {
-   // Code goes here
-
+  arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= num) {
+      return i;
+    }
+  }
+  return arr.length;
 }
 
+/* function whereIBelong(arr, num) {
+  var counter = 0;
+  for (i = 0; i < arr.length; i++) {
+    if (num > arr[i]) {
+      counter++;
+    }
+  }
+  return counter;
+} */
 
+// console.log(whereIBelong([], 10)); // 0
+// console.log(whereIBelong([1, 2, 3, 4], 1.5)); // 1
+// console.log(whereIBelong([4, 1, 7, 5, 3], 6)); // 4
+// console.log(whereIBelong([1, 2, 3, 4], 5)); // 5
 
-module.exports = whereIBelong
+module.exports = whereIBelong;
